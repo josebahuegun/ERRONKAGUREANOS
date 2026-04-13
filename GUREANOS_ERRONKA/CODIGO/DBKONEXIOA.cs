38f53b143fcd7504a9c7f5f45bb8b1722cfbd753
Unai Zabaleta <uzabaleta25@izarraitz.eus>
2026-04-13 12:40:31 +0200

    ezabatuGailuak eta gailuaGehitu funtzioen oinarriak egin ditut

--- GUREANOS_ERRONKA/CODIGO/DBKONEXIOA.cs
+++ GUREANOS_ERRONKA/CODIGO/DBKONEXIOA.cs
@@ -69,5 +69,66 @@ namespace GUREANOS_ERRONKA.CODIGO
             return num;
 
         }
+
+        static public int gailuaEzabatu(Gailua g)
+        {
+            int num;
+            KONEXIOA.Konektatu();
+            try
+            {
+                string sqlie = "DELETE FROM kontaktua where izena=@izena and telefonoa=@telefonoa;";
+                using (MySqlCommand komandue = new MySqlCommand(sqlie, KONEXIOA.konektatu))
+                {
+                    //komandue.Parameters.AddWithValue("@izena", g.Izena);
+                    //komandue.Parameters.AddWithValue("@telefonoa", g.Telefonoa);
+                    num = komandue.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                num = ex.Number;
+            }
+            finally
+            {
+                //deskonektatu
+                KONEXIOA.Deskonektatu();
+            }
+            //3.- Bueltatu zenbakia: 0 edo 1 querytik, edo bestela errore zenbakia          
+            return num;
+        }
+
+        static public int kontaktuaGehitu(Gailua g)
+        {
+            int num;
+            //1.- Konektatu
+            KONEXIOA.Konektatu();
+            //2. inserta egin
+            
+            try
+            {
+                //inserta, PARAMETRO BIDEZ, PRAKTIKA ONA
+                string sqlie = "INSERT INTO gailua (izena, telefonoa) VALUES (@izena, @telefonoa);";
+                //using erabilita komandue aldagia "hustu" egiten da agindua amaitzena, baina ez konexioa adibidez, nik egin behar close horrela jarrita
+                using (MySqlCommand komandue = new MySqlCommand(sqlie, KONEXIOA.konektatu))
+                {
+                    // Parametroak gehitu
+                    //komandue.Parameters.AddWithValue("@izena", g.Izena);
+                    //komandue.Parameters.AddWithValue("@telefonoa", g.Telefonoa);
+                    num = komandue.ExecuteNonQuery();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                num = ex.Number;
+            }
+            finally // try edo catchera joan, beti konexioa izteko. Beste aukera bat, hau gabe, return aurretik close
+            {
+                //deskonektatu
+                KONEXIOA.Deskonektatu();
+            }
+            //3.- Bueltatu zenbakia: 0 edo 1 querytik, edo bestela errore zenbakia          
+            return num;
+        }
+
     }
 }
