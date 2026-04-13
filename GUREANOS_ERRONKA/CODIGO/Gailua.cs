a47986b87d9e2f679469574872b4e0e4f96e1c0d
Unai Zabaleta <uzabaleta25@izarraitz.eus>
2026-04-13 10:43:15 +0200

    Gailua klasearen oinarria egin dut

--- GUREANOS_ERRONKA/CODIGO/Gailua.cs
+++ GUREANOS_ERRONKA/CODIGO/Gailua.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GUREANOS_ERRONKA.CODIGO
 {
-    internal class Gailua
+    public class Gailua
     {
-        public Gailua() 
-        { 
+        private int id;
+        private DateTime erosteData;
+        private string kokalekua;
+        private string mapa;
+        private bool aktibo;
+
+        public int Id { get => id; set => id = value; }
+        public DateTime ErosteData { get => erosteData; set => erosteData = value; }
+        public string Kokalekua { get => kokalekua; set => kokalekua = value; }
+        public string Mapa { get => mapa; set => mapa = value; }
+        public bool Aktibo { get => aktibo; set => aktibo = value; }
+
+        public Gailua(int ida, DateTime eData, string koka, string map, bool aktiboa)
+        {
+
+            id = ida;
+            erosteData = eData;
+            kokalekua = koka;
+            mapa = map;
+            aktibo = aktiboa;
         }
     }
 }
