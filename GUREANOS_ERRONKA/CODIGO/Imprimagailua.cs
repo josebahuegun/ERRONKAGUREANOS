cb78e3f66e765c336607d166d87f8612358326ea
Unai Zabaleta <uzabaleta25@izarraitz.eus>
2026-04-14 08:45:45 +0200

    imprimagailua klasea sortu dut, bertan gailuak ematen dion herentzia aplikatu dut eta konstruktore bat sortu diot

---
+++ GUREANOS_ERRONKA/CODIGO/Imprimagailua.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUREANOS_ERRONKA.CODIGO
+{
+    public class Inprimagailua : Gailua
+    {
+        private bool koloretakoa;
+        private string teknologia;
+
+        public bool Koloretakoa { get => koloretakoa; set => koloretakoa = value; }
+        public string Teknologia { get => teknologia; set => teknologia = value; }
+        public Inprimagailua(int ida, DateTime eData, string koka, string map, bool aktiboa, bool kolor, string tek)
+            : base(ida, eData, koka, map, aktiboa)
+        {
+            koloretakoa = kolor;
+            teknologia = tek;
+        }
+    }
+}
