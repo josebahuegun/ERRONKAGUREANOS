e263422a0d9f46065aeba62c5a11cdcdc2ff539b
Unai Zabaleta <uzabaleta25@izarraitz.eus>
2026-04-14 08:38:28 +0200

    Ordenagailua klasea sortu dut, bertan gailuarengatik jasotzen duen herentzia aplikatu dut eta konstruktore bat jarri diot

---
+++ GUREANOS_ERRONKA/CODIGO/Ordenagailua.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUREANOS_ERRONKA.CODIGO
+{
+    // : Gailua jarriz gero herentzia aplikatzen da eta Gailuaren propietateak jasotzen dira
+    public class Ordenagailua : Gailua
+    {
+        private string RAM;
+        private string ROM;
+        private string CPU;
+
+        public string RAM1 { get => RAM; set => RAM = value; }
+        public string ROM1 { get => ROM; set => ROM = value; }
+        public string CPU1 { get => CPU; set => CPU = value; }
+
+        // Konstruktorea: Gailuaren datuak + Ordenagailuaren datu espezifikoak
+        public Ordenagailua(int ida, DateTime eData, string koka, string map, bool aktiboa, string ram, string rom, string cpu)
+            : base(ida, eData, koka, map, aktiboa) // Aitaren konstruktoreari deitzen dio
+        {
+            RAM1 = ram;
+            ROM1 = rom;
+            CPU1 = cpu;
+        }
+    }
+}
